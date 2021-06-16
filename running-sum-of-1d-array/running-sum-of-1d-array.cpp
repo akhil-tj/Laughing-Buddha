0c00efa1d1308ec194feabc69440091931490a35
Akhil T J <itexpert2572000@gmail.com>
2021-06-16 22:19:43 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (33.82%) - LeetHub

---
+++ running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> v;
+        
+     for(int i=0; i<nums.size(); i++) {
+         int sum = 0;
+         for(int j=0; j<=i; j++) {
+            sum = sum+ nums[j];
+         }
+         v.emplace_back(sum);
+     }
+        return v;
+    }
+    
+};
