e77f9c80402e04b4a0d4d45c421d8e275db1bb3f
Akhil T J <itexpert2572000@gmail.com>
2021-07-10 16:43:43 +0530

    Time: 16 ms (13.51%), Space: 15.3 MB (39.73%) - LeetHub

---
+++ shuffle-string/shuffle-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string result = s;
+        for(int i=0; i<indices.size(); i++) {
+           result[indices[i]]=s[i];
+        }
+        return result;
+    }
+};
